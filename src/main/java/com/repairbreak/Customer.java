503486dfa1dc66ecea252153be7b9e4fe2f98724
Syed Farhan <syedrfarhan2@gmail.com>
2018-02-09 23:22:39 -0500

    Created Customer class

--- src/main/java/com/repairbreak/Customer.java
+++ src/main/java/com/repairbreak/Customer.java
@@ -1,4 +1,50 @@
 package com.repairbreak;
 
+import java.util.ArrayList;
+
 public class Customer {
+    private String name;
+    private String number;
+    private String email;
+    private List<Device> devices = new ArrayList<Device>();;
+
+    public Customer() {} // JPA only
+
+    public Customer(String name, String number, String email) {
+        this.name = name;
+        this.number = number;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Device> getDevices() {
+        return devices;
+    }
+
+    public void addDevice(Device device) {
+        devices.add(device);
+    }
 }
---
+++ src/main/java/com/repairbreak/Device.java
@@ -0,0 +1,4 @@
+package com.repairbreak;
+
+public class Device {
+}
