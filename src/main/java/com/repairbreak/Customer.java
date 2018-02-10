28494bad741c446b972b58426bbe946ff7150e0a
Syed Farhan <syedrfarhan2@gmail.com>
2018-02-10 00:01:37 -0500

    Created a generic request super class

--- src/main/java/com/repairbreak/Customer.java
+++ src/main/java/com/repairbreak/Customer.java
@@ -1,6 +1,7 @@
 package com.repairbreak;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Customer {
     private String name;
--- src/main/java/com/repairbreak/Request.java
+++ src/main/java/com/repairbreak/Request.java
@@ -1,4 +1,46 @@
 package com.repairbreak;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Request {
+    private Date opened;
+    private Date closed;
+    private String status;
+    private Map<Date, String> history = new HashMap<>();
+
+    public Request() {
+        opened = new Date();
+        status = "open";
+        history.put(opened, status);
+    }
+
+    public Date getOpened() {
+        return opened;
+    }
+
+    public Date getClosed() {
+        return closed;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setClosed() {
+        closed = new Date();
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Map<Date, String> getHistory() {
+        return history;
+    }
+
+    public void addHistory(String event) {
+        history.put(new Date(), event);
+    }
 }
